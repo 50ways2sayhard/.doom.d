5d90c0573002a8a31ab8a999b0dd0cd29fd6ca2c
50ways2sayhard <gjtzone@hotmail.com>
2021-05-06 15:32:24 +0800

    :sparkles: tree-sitter

---
+++ modules/private/tree-sitter/config.el
@@ -0,0 +1,8 @@
+;;; private/tree-sitter/config.el -*- lexical-binding: t; -*-
+
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+  )
---
+++ modules/private/tree-sitter/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/tree-sitter/packages.el
+
+(package! tree-sitter)
+(package! tree-sitter-langs)
